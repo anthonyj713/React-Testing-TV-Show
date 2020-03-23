85e2e478e78c076e5cb2e1d42494c0249ce5c990
Anthony Johnson <wontons713@Anthonys-MacBook-Pro.local>
2020-03-23 17:01:31 -0600

    some tests work, others not yet. trying to figure it out. have a distaste for testing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import Dropdown from "react-dropdown";
 import parse from "html-react-parser";
-
+import { fetchShow } from './components/fetchShow';
 import { formatSeasons } from "./utils/formatSeasons";
-
 import Episodes from "./components/Episodes";
 import "./styles.css";
 
@@ -15,16 +13,12 @@ export default function App() {
   const episodes = seasons[selectedSeason] || [];
 
   useEffect(() => {
-    const fetchShow = () => {
-      axios
-        .get(
-          "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
-        )
+    fetchShow()
         .then(res => {
+          console.log(res.data);
           setShow(res.data);
           setSeasons(formatSeasons(res.data._embedded.episodes));
         });
-    };
     fetchShow();
   }, []);
 
@@ -39,7 +33,7 @@ export default function App() {
   return (
     <div className="App">
       <img className="poster-img" src={show.image.original} alt={show.name} />
-      <h1>{show.name}</h1>
+      <h1 data-testid='title'>{show.name} </h1>
       {parse(show.summary)}
       <Dropdown
         options={Object.keys(seasons)}
